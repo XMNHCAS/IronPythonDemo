a7a9aa88a112752a047429d485a207dd5c5e1a04
XMNHCAS <1351669870@qq.com>
2023-01-29 14:13:20 +0800

    添加项目文件。

---
+++ IronPythonDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+
+namespace IronPythonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建python解释器
+            var engine = Python.CreateEngine();
+
+            SetSearchFile(ref engine);
+
+            // 加载脚本文件
+            dynamic py = engine.ExecuteFile(@"PythonScripts/main.py");
+
+            // 调用Python脚本的Test函数
+            Console.WriteLine("Test:");
+            var data = py.Test();
+            Console.WriteLine(data);
+
+            Console.WriteLine();
+
+            // 查看IronPython的Python版本及使用的.NET版本
+            Console.WriteLine("Python & .NET Version:");
+            var version = py.SysVersion();
+            Console.WriteLine(version);
+
+            Console.WriteLine();
+
+            // 使用Python的UUID标准库生成基于时间戳的UUID
+            Console.WriteLine("Create UUID By Python:");
+            var uuid = py.CreateUUID();
+            Console.WriteLine(uuid.ToString());
+
+            Console.WriteLine();
+
+            // 调用requests库
+            Console.WriteLine("Use Requests:");
+            var resp = py.GetReqTest();
+            Console.WriteLine(resp);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 配置python第三方库路径
+        /// </summary>
+        /// <param name="engine"></param>
+        private static void SetSearchFile(ref ScriptEngine engine)
+        {
+            var paths = engine.GetSearchPaths();
+            paths.Add(@"PythonScripts\Lib");
+            paths.Add(@"PythonScripts\Lib\site-packages");
+            engine.SetSearchPaths(paths);
+        }
+    }
+}
---
+++ IronPythonDemo/PythonScripts/main.py
@@ -0,0 +1,19 @@
+﻿import sys
+import uuid
+import requests as req
+
+def Test():
+    return 'Hello IronPython!'
+
+
+def SysVersion():
+    return sys.version
+
+
+def CreateUUID():
+    return str(uuid.uuid1())
+
+
+def GetReqTest():
+    res = req.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
+    return res.text
